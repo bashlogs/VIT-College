7574fae6805825bdf56813349e243fbd7a14bae3
MK-C0dE5 <mayurkhadde49@gmail.com>
2022-12-23 23:00:31 +0000

    InOrder

--- DS/tree/tree1.c
+++ DS/tree/tree1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define bool int;
+#define bool int
 
 struct tree{
   int data;
@@ -19,32 +19,10 @@ struct tree* newnode(int a){
   new->left = NULL;
   new->right = NULL;
   return new;
-}
+};
 
-bool empty(struct stact* top){
-  return (top == NULL ? 1 : 0);
-}
-
-void inOrder(struct tree *root){
-  struct tree *current = root;
-  struct stack *s = NULL;
-  bool done = 0;
-  while(!done){
-    if(current != NULL){
-      push(&s, current);
-      current = current->left;
-    }
-    else{
-      if(!isEmpty(s)){
-        current = pop(&s);
-        printf("%d",current->data);
-        current = current->right;
-      }
-      else{
-        done = 1;
-      }
-    }
-  }
+bool isEmpty(struct stack* top){
+  return (top == NULL)? 1 : 0;
 }
 
 struct tree* pop(struct stack** ref){
@@ -65,7 +43,7 @@ struct tree* pop(struct stack** ref){
 }
 
 void push(struct stack** ref, struct tree *t){
-  struct stact newNode = malloc(sizeof(struct stack));
+  struct stack* newNode = malloc(sizeof(struct stack));
   if(newNode==NULL){
     printf("Stack Overflow");
     getchar();
@@ -76,6 +54,28 @@ void push(struct stack** ref, struct tree *t){
   *ref = newNode;
 }
 
+void inOrder(struct tree *root){
+  struct tree *current = root;
+  struct stack *s = NULL;
+  bool done = 0;
+  while(!done){
+    if(current != NULL){
+      push(&s, current);
+      current = current->left;
+    }
+    else{
+      if(!isEmpty(s)){
+        current = pop(&s);
+        printf("%d\t",current->data);
+        current = current->right;
+      }
+      else{
+        done = 1;
+      }
+    }
+  }
+}
+
 int main(){
   struct tree* head = newnode(1);
   head->left = newnode(2);
@@ -84,8 +84,8 @@ int main(){
   head->left->right = newnode(5);
   head->right->left = newnode(6);
   head->right->right = newnode(7);
-
-
+  printf("InOrder List: ");
+  inOrder(head);
   getchar();
   return 0;
 }
