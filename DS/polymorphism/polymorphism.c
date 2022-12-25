8ebf8af77e245472e90eac4fcc9604314fdcb682
MK-C0dE5 <mayurkhadde49@gmail.com>
2022-12-25 21:06:27 +0000

    InOrder

---
+++ DS/polymorphism/polymorphism.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct poly{
+  int data;
+  int raise;
+  struct poly* next;
+}
+
+int main(){
+  struct poly* p1 = NULL;
+  struct poly* p2 = NULL;
+
+
+  return 0;
+}
